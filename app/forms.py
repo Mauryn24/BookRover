7a6eefd3967ab5854f1a4b7072fc0d04a094bd69
Mauryn24 <johndoe@example.com>
2023-12-18 09:38:39 +0300

    user authentication

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length,Email, EqualTo
-
+from wtforms.validators import DataRequired, Length,Email, EqualTo, ValidationError
+from app.models import User, Book
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -10,8 +12,43 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That Username is taken. Please choose a different one')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one')
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=10)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+        
+    # create a function for username validation
+    def validate_username(self, username):
+        # from app.models import User
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another username')
+        
+    # create a function for email validation
+    def validate_email(self, email):
+        # from app.models import User
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose another email')    
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, url_for, redirect, flash
-from app.forms import RegistrationForm, LoginForm
+import os
+import secrets
+from PIL import Image
+from flask import Flask, render_template, url_for, redirect, flash, request
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Book
 from app import app, db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
 
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    return render_template('home.html')
 @app.route('/register/', methods=('GET', 'POST'))
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_paasword = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -17,13 +28,57 @@ def register():
 
 @app.route('/login/', methods=('GET', 'POST'))
 def login():
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'You have been logged in!', 'success')
-        return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        # checking if the user exists and password is valid
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please Check email and Password', 'danger')
+        # return redirect(url_for('home'))
     return render_template('login.html', title='Login', form=form)
 
-@app.route('/')
-@app.route('/home/')
-def home():
-    return render_template('home.html')
+@app.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/pictures/', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    
+    i.save(picture_path)
+
+    return picture_fn
+@login_required
+@app.route('/account/', methods=['GET', 'POST'])
+def account():
+        # creating an instance of the form
+        form = UpdateAccountForm()
+        if form.validate_on_submit():
+            if form.picture.data:
+                picture_file = save_picture(form.picture.data)
+                current_user.image_file = picture_file
+            current_user.username = form.username.data
+            current_user.email = form.email.data
+            db.session.commit()
+            flash('Your Account has been updated!', 'success')
+            return redirect(url_for('account'))
+        elif request.method == 'GET':
+            form.username.data = current_user.username
+            form.email.data = current_user.email
+        image_file = url_for('static', filename='pictures/' + current_user.image_file)
+        return render_template('account.html',
+                               title='Account', image_file=image_file,
+                               form=form)
